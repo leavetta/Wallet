e10d4cc2b5fbf0c63b734922bcd5f2130ba81a64
leavetta <47735624+leavetta@users.noreply.github.com>
2024-10-22 16:55:29 +0700

    Add users to dbContext

--- WalletAspNetCore/WalletAspNetCore.DataBaseOperations/EFStructures/ApplicationDbContext.cs
+++ WalletAspNetCore/WalletAspNetCore.DataBaseOperations/EFStructures/ApplicationDbContext.cs
@@ -21,6 +21,7 @@ namespace WalletAspNetCore.DataBaseOperations.EFStructures
         public DbSet<Balance> Balances { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<User> Users { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
